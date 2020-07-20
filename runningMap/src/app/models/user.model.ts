6d7c25f9aa33f16dbcc1f9cefb2416700521eb11
caroline-ctrl <duon.caroline@gmail.com>
2020-07-20 11:36:02 +0200

    implement service et model user

---
+++ runningMap/src/app/models/user.model.ts
@@ -0,0 +1,13 @@
+export class User {
+    constructor(
+        public avatar: string,
+        public firstname: string,
+        public lastname: string,
+        public pseudo: string,
+        public mail: string,
+        public city: string,
+        public gender: string,
+        public age: number,
+        public password: string,
+    ){}
+}; 
--- runningMap/src/app/services/user.service.ts
+++ runningMap/src/app/services/user.service.ts
@@ -1,9 +1,61 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private URL_API = 'http://localhost:3000';
+
+  constructor(private http: HttpClient) { }
+
+  getAllUser() {
+    return this.http.get<User[]>(this.URL_API + '/users');
+  }
+
+  getUserById(id: string) {
+    return this.http.get<User>(this.URL_API + '/user/' + id);
+  }
+
+  createUser(data) {
+    return this.http.post<User>(this.URL_API + '/create/' , data);
+  }
+
+  updateUser(id, data) {
+    return this.http.put<User>(this.URL_API + '/update/' + id, data);
+  }
+
+  uploadAvatar(id, data) {
+    return this.http.put<User>(this.URL_API + '/update/' + id, data);
+  }
+
+  updatePassword(data) {
+    return this.http.put<User>(this.URL_API + '/newPassword/', data);
+  }
+
+  delete(id) {
+    return this.http.delete<User>(this.URL_API + '/delete/' + id);
+  }
+
+  login(data) {
+    return this.http.post<User>(this.URL_API + '/log/', data);
+  }
+  
+  getUserByPseudo(data) {
+    return this.http.post<User>(this.URL_API + '/monCompte/', {"pseudo": data});
+  }
+
+  mail(data) {
+    return this.http.post<User>(this.URL_API + '/mail/', {"mail": data});
+  }
+
+  verifyCode(data) {
+    return this.http.post<User>(this.URL_API + '/verifyCode/', {"mail": data});
+  }
+
+  changePassword(data) {
+    return this.http.put<User>(this.URL_API + '/forgetPassword/', data);
+  }
 }
