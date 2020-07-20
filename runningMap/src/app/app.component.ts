0dd2151bacf6bfcb64baf5e90b1bee5718e85ea0
caroline-ctrl <duon.caroline@gmail.com>
2020-07-20 21:25:50 +0200

    implement app.component.ts navbar

--- runningMap/src/app/app.component.ts
+++ runningMap/src/app/app.component.ts
@@ -10,11 +10,14 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+  navigate: any;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar
   ) {
+    this.sideMenu();
     this.initializeApp();
   }
 
@@ -23,5 +26,31 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+
+  sideMenu(){
+    this.navigate = [
+      {
+        title: 'Home',
+        url: '/home',
+        icon: 'home-outline'
+      },
+      {
+        title: 'Itineraire = Aller-retour',
+        url: '/roundTrip',
+        icon: 'trail-sign-outline'
+      },
+      {
+        title: 'Itineraire = Boucle',
+        url: '/loop',
+        icon: 'infinite-outline'
+      },
+      {
+        title: 'Mon compte',
+        url: '/monCompte',
+        icon: 'construct-outline'
+      }
+    ];
   }
 }
