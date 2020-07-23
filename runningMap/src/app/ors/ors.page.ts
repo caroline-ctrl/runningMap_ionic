973cefa69b91f0f6d29f000458c0a928e1b2a761
caroline-ctrl <duon.caroline@gmail.com>
2020-07-23 10:42:06 +0200

    implement service

--- runningMap/src/app/ors/ors.page.ts
+++ runningMap/src/app/ors/ors.page.ts
@@ -1,4 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { OrsService } from './ors.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import * as L from 'leaflet';
+
+const iconRetinaUrl = 'assets/marker-icon-2x.png';
+const iconUrl = 'assets/marker-icon.png';
+const shadowUrl = 'assets/marker-shadow.png';
+const iconDefault = L.icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [ 25, 41 ],
+  iconAnchor: [ 12, 41 ],
+  popupAnchor: [ 1, -34 ],
+  tooltipAnchor: [ 16, -28 ],
+  shadowSize: [ 41, 41 ]
+});
+L.Marker.prototype.options.icon = iconDefault;
 
 @Component({
   selector: 'app-ors',
@@ -6,10 +25,193 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ors.page.scss'],
 })
 export class OrsPage implements OnInit {
+  itineraire: FormGroup;
+  // reactivForm
+  startingPoint;
+  endPoint;
+  // valeur coordonnées gps start
+  longitudeStart;
+  latitudeStart;
+  // valeur coordonnées gps end
+  longitudeEnd;
+  latitudeEnd;
+  // convertion en string
+  pointsstartLon;
+  pointsstartLat;
+  pointsEndLong;
+  pointsEndLat;
+  // coordonnées string depart
+  start;
+  end;
+  // map
+  mymap;
+  mymapItineraire;
+  // tableau coordonnées
+  latlngs;
+  // choix du sport
+  choice;
+  tablePoints;
+  // temps entre le point A et le point B
+  duration;
 
-  constructor() { }
+  constructor(
+    private orsService: OrsService,
+    private formBuilder: FormBuilder,
+  ) { }
 
   ngOnInit() {
+    this.itineraire = this.formBuilder.group({
+      startingPoint: [ '', Validators.required ],
+      endPoint: [ '', Validators.required ],
+      choiceValue: [ '', Validators.required ]
+    });
+
+    this.mymap = L.map('map').setView([ 43.6112422, 3.8767337 ], 15);
+
+    L.tileLayer('//{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+      maxZoom: 18
+    }).addTo(this.mymap);
+  }
+
+
+    // recupère les coordonnées GPS du point A
+  // return string
+  getStartingPoint() {
+    const formValue = this.itineraire.value;
+    const data = formValue.startingPoint;
+
+    this.orsService.getStartingPoint(data).subscribe(
+      (resultStart) => {
+        const coordinates =
+          resultStart['features']['0']['geometry']['coordinates'];
+        this.longitudeStart = coordinates['0'];
+        this.latitudeStart = coordinates['1'];
+        this.pointsstartLon = this.longitudeStart + '';
+        this.pointsstartLat = this.latitudeStart + '';
+
+        this.start = this.pointsstartLon + ',' + this.pointsstartLat;
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+
+  // choix du sport
+  getChoiceValue() {
+    const formValue = this.itineraire.value;
+    return (this.choice = formValue.choiceValue);
+  }
+
+  // recupère les coordonnées GPS du point B
+  getEndPoint() {
+    const formValue = this.itineraire.value;
+    const dataEnd = formValue.endPoint;
+
+    this.orsService.getEndPoint(dataEnd).subscribe(
+      (resultEnd) => {
+        const coordinates =
+          resultEnd['features']['0']['geometry']['coordinates'];
+        this.longitudeEnd = coordinates['0'];
+        this.latitudeEnd = coordinates['1'];
+        this.pointsEndLong = this.longitudeEnd + '';
+        this.pointsEndLat = this.latitudeEnd + '';
+        this.end = this.pointsEndLong + ',' + this.pointsEndLat;
+
+        this.direction(this.getChoiceValue(), this.start, this.end);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+
+  // récup les points du point A au point B
+  pointsArray(items) {
+    let pointsArray = [];
+    items.forEach((item) => {
+      pointsArray.push(new L.LatLng(item[1], item[0]));
+    });
+    return pointsArray;
+  }
+
+
+  // modifie l'orde dans le tableau
+  pointsLatLong(items) {
+    let pointsArray = [];
+    items.forEach((item) => {
+      pointsArray.push(item[0], item[1]);
+    });
+    return pointsArray;
+  }
+
+  // permet de réaliser le tracé de l'itineraire
+  direction(locomotion, start, end) {
+    this.orsService.direction(locomotion, start, end).subscribe(
+      (result) => {
+        if (this.mymap) {
+          // si la carte est deja utilisé
+          this.mymap.eachLayer((layer) => {
+            layer.remove();
+          });
+          this.mymap.remove();
+          this.mymap = null;
+          // si la carte est deja utilisé
+
+          this.mymapItineraire = L.map('map').setView(
+            [ this.latitudeStart, this.longitudeStart ],
+            18
+          );
+
+          L.tileLayer('//{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+            attribution:
+              '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+            maxZoom: 18
+          }).addTo(this.mymapItineraire);
+        }
+
+        this.tablePoints = [];
+        this.tablePoints = result['features']['0']['geometry']['coordinates'];
+
+        const polyline = L.polyline(this.pointsArray(this.tablePoints), {
+          color: 'red'
+        }).addTo(this.mymapItineraire);
+        this.mymapItineraire.fitBounds(polyline.getBounds());
+
+        L.marker([ this.latitudeStart, this.longitudeStart ]).addTo(
+          this.mymapItineraire
+        );
+        L.marker([ this.latitudeEnd, this.longitudeEnd ]).addTo(
+          this.mymapItineraire
+        );
+
+        this.matrix(this.getChoiceValue(), this.tablePoints);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
+  }
+
+  // temps et distance entre le point A et le point B
+  matrix(locomotion, points) {
+    let locationData = {
+      locations: points,
+      metrics: ['distance', 'duration']
+    };
+    this.orsService.matrix(locomotion, locationData).subscribe(
+      (result) => {
+        console.log(result);
+        const totalDuration = result['durations']['0']
+        const minute = (totalDuration[totalDuration.length-1])/60;
+        this.duration = minute.toFixed(2);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
   }
 
 }
--- runningMap/src/app/ors/ors.service.ts
+++ runningMap/src/app/ors/ors.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json',
+    'Authorization': '5b3ce3597851110001cf6248e4f4182f661b4d95829edd912435b2f4'
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrsService {
+  private URL_API_ORS = 'https://api.openrouteservice.org';
+  private API_KEY = '5b3ce3597851110001cf6248e4f4182f661b4d95829edd912435b2f4';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getStartingPoint(data) {
+    return this.http.get(this.URL_API_ORS + '/geocode/search?api_key=' + this.API_KEY + '&text=' + data);
+  }
+
+  getEndPoint(data) {
+    return this.http.get(this.URL_API_ORS + '/geocode/search?api_key=' + this.API_KEY + '&text=' + data);
+  }
+
+  direction(locomotion, start, end) {
+    return this.http.get(this.URL_API_ORS + '/v2/directions/' + locomotion + '?api_key=' + this.API_KEY + '&start=' + start + '&end=' + end);
+  }
+
+  matrix(locomotion, data) {
+    return this.http.post(this.URL_API_ORS + '/v2/matrix/' + locomotion, data,  httpOptions);
+  }
 }
