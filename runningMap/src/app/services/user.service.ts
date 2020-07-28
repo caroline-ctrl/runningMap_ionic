53d380c0069b1fbbedd2ffa9d25e24b3250f830b
caroline-ctrl <duon.caroline@gmail.com>
2020-07-28 15:33:41 +0200

    modif bg

--- runningMap/src/app/services/user.service.ts
+++ runningMap/src/app/services/user.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-//import { HttpClient } from '@angular/common/http';
 import { User } from '../models/user.model';
 import { HTTP } from '@ionic-native/http/ngx';
 
