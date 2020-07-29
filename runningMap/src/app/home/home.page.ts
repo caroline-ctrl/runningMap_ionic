e627e4ffdde57aa1cf5282f05ee4ca3e8d30d003
caroline-ctrl <duon.caroline@gmail.com>
2020-07-29 11:24:26 +0200

    ajout des demandes de permission dans la home page et suppression dans la page ors.page

--- runningMap/src/app/home/home.page.ts
+++ runningMap/src/app/home/home.page.ts
@@ -3,11 +3,13 @@ import { MenuController } from '@ionic/angular';
 import { Storage } from '@ionic/storage';
 import { Router, NavigationEnd } from '@angular/router';
 import { AlertController } from '@ionic/angular';
+import { AndroidPermissions } from '@ionic-native/android-permissions/ngx';
+import { LocationAccuracy } from '@ionic-native/location-accuracy/ngx';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: [ 'home.page.scss' ]
 })
 export class HomePage implements OnInit {
   dataStorage;
@@ -18,14 +20,20 @@ export class HomePage implements OnInit {
     private storage: Storage,
     private router: Router,
     public alertController: AlertController,
+    private androidPermissions: AndroidPermissions,
+    private locationAccuracy: LocationAccuracy
   ) {}
 
-  ngOnInit(){
-    this.storage.get('pseudo').then(val => {
+  ngOnInit() {
+    this.storage.get('pseudo').then((val) => {
       this.dataStorage = val;
     });
+
+    this.checkGPSPermission();
+    this.requestGPSPermission();
+    this.askToTurnOnGPS();
   }
-  
+
   openFirst() {
     this.menu.enable(true, 'first');
     this.menu.open('first');
@@ -38,14 +46,13 @@ export class HomePage implements OnInit {
     this.actualyNabBar();
   }
 
-
   // actualiser la navbar
   actualyNabBar() {
-    this.router.routeReuseStrategy.shouldReuseRoute = (() => {
+    this.router.routeReuseStrategy.shouldReuseRoute = () => {
       return false;
-    });
+    };
     this.subscribe = this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd){
+      if (event instanceof NavigationEnd) {
         this.router.navigated = false;
       }
     });
@@ -57,8 +64,70 @@ export class HomePage implements OnInit {
       cssClass: 'my-custom-class',
       header: 'Deconnexion',
       message: 'Vous êtes bien déconnecté',
-      buttons: ['OK']
+      buttons: [ 'OK' ]
     });
     await alert.present();
+  }
+
+  // Verifie si permission acces GPS
+  checkGPSPermission() {
+    this.androidPermissions
+      .checkPermission(this.androidPermissions.PERMISSION.ACCESS_FINE_LOCATION)
+      .then(
+        (result) => {
+          if (result.hasPermission) {
+            this.askToTurnOnGPS();
+          } else {
+            this.requestGPSPermission();
+          }
+        },
+        (err) => {
+          alert(err);
+        }
+      );
+  }
+
+  // Obtenir l'autorisation de localisation de l'utilisateur
+  requestGPSPermission() {
+    this.locationAccuracy.canRequest().then((canRequest: boolean) => {
+      if (canRequest) {
+        this.locationAccuracy
+          .request(this.locationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY)
+          .then(() => {
+            console.log('success'), (err) => console.log(err);
+          });
+      } else {
+        this.androidPermissions
+          .requestPermission(
+            this.androidPermissions.PERMISSION.ACCESS_COARSE_LOCATION
+          )
+          .then(
+            () => {
+              this.askToTurnOnGPS();
+            },
+            (error) => {
+              alert(
+                'requestPermission Error requesting location permissions ' +
+                  error
+              );
+            }
+          );
+      }
+    });
+  }
+
+  // si on a l'autorisation d'accés à la localisation : ouvre la boite de dialogue
+  askToTurnOnGPS() {
+    this.locationAccuracy
+      .request(this.locationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY)
+      .then(
+        () => {
+          console.log('permission ok');
+        },
+        (error) =>
+          alert(
+            'Error requesting location permissions ' + JSON.stringify(error)
+          )
+      );
   }
 }
